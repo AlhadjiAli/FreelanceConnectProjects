6be83f97e6d76521d525b329fc347ba5c6229df1
AbdoulayeTheSpy <njigouhrazak@iut-dhaka.edu>
2025-04-09 14:17:40 +0200

    remove slider

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b563ff4b50afe1cb"; }
+/******/ 		__webpack_require__.h = function() { return "d5bdf3b66804539d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
